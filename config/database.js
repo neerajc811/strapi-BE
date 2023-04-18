cf1951d95e533816b0ee9b6e5b03e1e76461893c
ERPDADDY <neerajc811@gmail.com>
2023-04-18 14:51:09 +0530

    environment set for production and development

--- config/database.js
+++ config/database.js
@@ -1,33 +1,15 @@
 // strapi-api/config/database.js
-if (process.env.NODE_ENV === 'development')
-{
-    const path = require('path');
+
+const path = require('path');
 
   module.exports = ({ env }) => ({
     connection: {
       client: 'sqlite',
       connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '')),
       },
       useNullAsDefault: true,
     },
   });
-}
-else{
-  module.exports = ({ env }) => ({
-    connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'B2B'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'root'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        
-      },
-      
-    }
-  });
-}
+
 
---
+++ config/env/production/database.js
@@ -0,0 +1,16 @@
+const path = require('path');
+module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      NODE_ENV: 'production',
+      connection: {
+        host: env('DATABASE_HOST', ''),
+        port: env.int('DATABASE_PORT', ''),
+        database: env('DATABASE_NAME', ''),
+        user: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
+        
+      },
+      
+    }
+  });
