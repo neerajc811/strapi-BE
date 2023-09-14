d3fe8ec0e54f48edf9d7f83f0d75152e9d896a18
Neeraj <neerajc811@gmail.com>
2023-09-14 12:44:28 +0530

    REmove b2b frontend and backend

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,29 @@
-const path = require('path');
-module.exports = ({ env }) => ({
-    connection: {
-      client: 'postgres',
-      NODE_ENV: 'production',
-      connection: {
-        host: env('DATABASE_HOST', ''),
-        port: env.int('DATABASE_PORT', ''),
-        database: env('DATABASE_NAME', ''),
-        user: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+// const path = require('path');
+// module.exports = ({ env }) => ({
+//     connection: {
+//       client: 'postgres',
+//       NODE_ENV: 'production',
+//       connection: {
+//         host: env('DATABASE_HOST', ''),
+//         port: env.int('DATABASE_PORT', ''),
+//         database: env('DATABASE_NAME', ''),
+//         user: env('DATABASE_USERNAME', ''),
+//         password: env('DATABASE_PASSWORD', ''),
         
-      },
+//       },
       
-    }
+//     }
+//   });
+
+const path = require('path');
+
+  module.exports = ({ env }) => ({
+    connection: {
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '')),
+      },
+      useNullAsDefault: true,
+    },
   });
+
