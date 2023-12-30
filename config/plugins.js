c9a73afe2332ed05f0d36279571d2c1ab53996cb
Neeraj <neerajc811@gmail.com>
2023-12-30 18:42:08 +0530

    30-12-2023

---
+++ config/plugins.js
@@ -0,0 +1,30 @@
+module.exports = ({ env }) => ({
+    // ...
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('SMTP_HOST', 'smtp.gmail.com'),
+          port: env('SMTP_PORT', 465),
+          auth: {
+            user: 'neerajc811@gmail.com',
+            pass: 'eond ivej klnz afer',
+          },
+          // ... any custom nodemailer options
+        },
+        settings: {
+          defaultFrom: 'neerajc811@gmail.com',
+          
+        },
+      },
+    },
+    upload: {
+      config: {
+        provider: 'local',
+        providerOptions: {
+          limit: 52428800,
+        },
+      },
+    },
+    // ...
+  });
