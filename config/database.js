3e67fcdc6fbd60f1ffa234453d0566952f90e768
neeraj choudhary <nrcneeraj811@gamil.com>
2024-05-16 12:49:59 +0530

    16-05-2024

--- config/database.js
+++ config/database.js
@@ -1,15 +1,40 @@
-// strapi-api/config/database.js
-
 const path = require('path');
 
-  module.exports = ({ env }) => ({
-    connection: {
-      client: 'sqlite',
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'postgres');
+
+  const connections = {
+
+    postgres: {
       connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '')),
+        
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'UGSales_Testing'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'root'),
+        
+        schema: env('DATABASE_SCHEMA', 'public'),
       },
-      useNullAsDefault: true,
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
-  });
+    // sqlite: {
+    //   connection: {
+    //     filename: path.join(
+    //       __dirname,
+    //       '..',
+    //       env('DATABASE_FILENAME', '.tmp/data.db')
+    //     ),
+    //   },
+    //   useNullAsDefault: true,
+    // },
+  };
 
-
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
