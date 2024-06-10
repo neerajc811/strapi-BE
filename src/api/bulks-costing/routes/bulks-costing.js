29fe09cfc38f864704173b584fd8e3b595348677
Neeraj <neerajc811@gmail.com>
2024-06-10 18:30:25 +0530

    10-06-2024

---
+++ src/api/bulks-costing/routes/bulks-costing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bulks-costing router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::bulks-costing.bulks-costing');
---
+++ src/api/bulks-costing/services/bulks-costing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bulks-costing service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::bulks-costing.bulks-costing');
